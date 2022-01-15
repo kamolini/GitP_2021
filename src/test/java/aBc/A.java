53893401575f0686f6a99bcec30a84f8df7a25de
seraj <msk6791@yahoo.com>
2022-01-15 17:53:19 -0500

    three files added

---
+++ src/test/java/aBc/A.java
@@ -0,0 +1,13 @@
+package aBc;
+
+public abstract class A {
+
+	public abstract void sum();
+	
+	
+	public void sum1() {
+		System.out.println("From A");
+	}
+	
+	
+}
---
+++ src/test/java/aBc/Test1.java
@@ -0,0 +1,17 @@
+package aBc;
+
+public class Test1 {
+
+	public static void main(String[] agrs) {
+	A a=new A() {
+	
+	public void sum() {
+		System.out.println("I live in USA");
+	}
+
+};
+a.sum();
+a.sum1();
+}
+}
+
